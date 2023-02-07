c0b72727c040fb37baf379c88f025e977972db47
Challenger FV4034 <challenger@Cossack>
2023-02-07 06:14:13 -0500

    Long messages are now split into multiple messages in a more readable format (words don't get cut off anymore) and added try-catch error

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='', intents=intents)
 
+def split_string(input_string):
+    split_strings = []
+    start = 0
+    while start < len(input_string):
+        end = start + 1900
+        if end < len(input_string):
+            while input_string[end] != ' ':
+                end -= 1
+        split_strings.append(input_string[start:end])
+        start = end + 1
+    return split_strings
 
 @bot.event
 async def on_ready():
@@ -26,19 +37,27 @@ async def on_message(message):
         if message.channel.name == 'chatgpt':
             quotes = "```"
         loading_message = await message.channel.send('```json\n"ChatGPT is generating a response....."\n```')#, reference=message)
-        data = await gpt.ask(message.content)
-        res = data["choices"][0]["text"]
-        # if res[0] == " ":
-            # res = res[1:]
-        res = res.lstrip()
-        await loading_message.delete()
-        if (len(res) > 1900):
-            chunks = [res[i:i+1900] for i in range(0, len(res), 1900)]
-            for chunk in chunks:
-                await message.channel.send(quotes + chunk + quotes)
-                # await message.channel.send(chunk)
-        else:
-            await message.channel.send(quotes + res + quotes)
-            # await message.channel.send(res)
+        try:
+            data = await gpt.ask(message.content)
+            res = data["choices"][0]["text"]
+            # if res[0] == " ":
+                # res = res[1:]
+            res = res.lstrip()
+            await loading_message.delete()
+            if (len(res) > 1900):
+                chunks1 = [res[i:i+1900] for i in range(0, len(res), 1900)]
+                for chunk in chunks1:
+                    print(chunk)
+                    print("---")
+                chunks = split_string(res)
+                for chunk in chunks:
+                    await message.channel.send(quotes + chunk + quotes)
+                    # await message.channel.send(chunk)
+            else:
+                await message.channel.send(quotes + res + quotes)
+                # await message.channel.send(res)
+        except Exception as e:
+            await message.channel.send("```An error has occured.. Try again or contact Michael```")
+            print("An error occurred:", e)
 
 bot.run(os.environ["DISCORD_TOKEN"])
