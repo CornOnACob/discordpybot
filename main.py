e4411786314dd7d7263fbe8f2227e299183b0da4
Challenger FV4034 <challenger@Cossack>
2023-02-07 03:03:39 -0500

    The bot now only works in the bot channel and responds to the sl command with a train emote

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import os
 import discord
 from discord.ext import commands
 
+# from dotenv import load_dotenv
+# load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='', intents=intents)
 
 
 @bot.event
@@ -19,8 +21,16 @@ async def ping(ctx):
     await ctx.send('pong')
 
 @bot.command()
+async def knock(ctx):
+    await ctx.send('who is there')
+
+@bot.command()
 async def hello(ctx):
     await ctx.send("Choo choo! 🚅")
 
+@bot.event
+async def on_message(message):
+    if message.content == 'sl' and message.channel.name == 'bot':
+        await message.channel.send('🚅')
 
 bot.run(os.environ["DISCORD_TOKEN"])
